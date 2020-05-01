13b9382f87faa43cd3b4288fb5060400656193b4
Александр <56562683+AlexBessonov@users.noreply.github.com>
2020-05-01 23:02:01 +0300

    Add files via upload

---
+++ build/precache-manifest.248e1f95f55eeb3c3ba66467cef31608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043374722ce3ba7c2221275020dce64f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "824caf218121b35c9726",
+    "url": "/static/css/main.3114cb68.chunk.css"
+  },
+  {
+    "revision": "920678087abcb4f9d2b7",
+    "url": "/static/js/2.58098b96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.58098b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824caf218121b35c9726",
+    "url": "/static/js/main.bbac52ba.chunk.js"
+  },
+  {
+    "revision": "19a239c6bf56f16064f9",
+    "url": "/static/js/runtime-main.d041ebb8.js"
+  }
+]);
